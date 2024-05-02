eb5814f2cf246f3cecf080e7061f8d5d76e9383d
sukem124 <s.kadibalban@iem.uni-kiel.de>
2024-05-02 14:21:21 +0200

    the user can use the weighing method they want (min or multi)

--- R/4_relation_per_sample.R
+++ R/4_relation_per_sample.R
@@ -3,6 +3,7 @@
 #' The prediction of the frequency of ecological relationship between pairs of co-grown bacteria in each sample (community)
 #' as exported by the function make_eco_mat and a user imported OTU table
 #'
+#' @param weighing_method either "min" for using lower abundance of the interacting bacteria for whighing (default), or "multi" for using the multiplication of the two abundances.
 #' @param OTU_table a matrix of the abundances of each species in each sample (columns are samples and rows are bacterial species)
 #' @param eco_mat a list, as exported by the function make_eco_mat and which contains: matrix of all_models X all_models with each cell represents the ecological relation between the two strains
 #' @return
@@ -14,7 +15,7 @@
 #'
 #' @export
 #'
-relation_per_sample <- function(OTU_table, eco_mat){
+relation_per_sample <- function(OTU_table, eco_mat, weighing_method="min"){
   relations <- unique(unlist(eco_mat))
   relations <- relations[relations != "Alone"]
   relations_per_samp <- as.data.frame(matrix(0, ncol(OTU_table), length(relations)))
@@ -32,16 +33,23 @@ relation_per_sample <- function(OTU_table, eco_mat){
     }
     bac_in_samp <- rownames(OTU_table)[bac_ind]
     samp_abund <- data.frame(species = bac_in_samp, abund = OTU_table[bac_in_samp,id])
-    #  samp_abund <- samp_abund[order(samp_abund$abund, decreasing = T),]
     samp_abund <- samp_abund[order(samp_abund$abund),]
     rownames(samp_abund) <- samp_abund$species
-    bac_in_samp[!bac_in_samp %in% names(eco_mat)]
     samp_eco_mat <- eco_mat[bac_in_samp, bac_in_samp]
     samp_abund_mat <- samp_eco_mat
     samp_abund_mat[] <- 0
-    for (sp in rownames(samp_abund)) {
-      samp_abund_mat[sp,] <- samp_abund[sp, "abund"]
-      samp_abund_mat[,sp] <- samp_abund[sp, "abund"]
+    sp <- rownames(samp_abund)[1]
+    while(length(bac_in_samp)>0){
+      sp1 <- bac_in_samp[1]
+      bac_in_samp <- bac_in_samp[-1]
+      for (sp2 in bac_in_samp){
+        if(args[[3]]=="min"){
+          samp_abund_mat[sp1,sp2] <- min(samp_abund[sp1, "abund"], samp_abund[sp2, "abund"])
+        }
+        else{
+          samp_abund_mat[sp1,sp2] <- samp_abund[sp1, "abund"]*samp_abund[sp2, "abund"]
+        }
+      }
     }
     for (relat in relations) {
       ind <- samp_eco_mat == relat
